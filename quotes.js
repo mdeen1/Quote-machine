3315e7733d74da469a118d207e87dae05a2b787f
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2018-06-09 17:05:13 -0700

    Add files via upload

--- quotes.js
+++ quotes.js
@@ -29,7 +29,7 @@ var quotes = [
     author: "Arya Srark" 
     },
      {
-       quote: "Chaos isn’t a pit. Chaos is a ladder. Many who try to climb it fail, and never get to try again — the fall breaks them. And some are given a chance to climb, but they refuse. They cling to the realm, or the gods, or love ... illusions. Only the ladder is real, the climb is all there is." ",
+       quote: "Chaos isn’t a pit. Chaos is a ladder. Many who try to climb it fail, and never get to try again — the fall breaks them. And some are given a chance to climb, but they refuse. They cling to the realm, or the gods, or love ... illusions. Only the ladder is real, the climb is all there is." ,
     author: "Petyr 'Littlefinger' Baelish" 
     },
     {
@@ -50,18 +50,33 @@ var displayedQuote;
 var currentIdx;
 
 function updateQuote() {
+    var idx = Math.floor(Math.random() * quotes.length);
+    displayedQuote = quotes[idx];
+}
+
+function updateQuoteSafe() {
     do{
         var idx = Math.floor(Math.random() * quotes.length); 
-    }while(currentIdx === idx);
-   
+    } while(idx === currentIdx);
     displayedQuote = quotes[idx];
-    
     currentIdx = idx;
-    $('#quote').html(quotes[idx].quote);
-    $('#author').html(quotes[idx].author);
+    
+    $('#quote').html(displayedQuote.quote);
+    $('#author').html(displayedQuote.author);
+}
+function shareTweet(){
+ var quoteToTweet = quotes[currentIdx].quote;
+ console.log(quoteToTweet.length > 100);
+ if(quoteToTweet.length > 100){
+     quoteToTweet = quoteToTweet.substr(0, 100).match(/(^.+)\s/)[1] + "...";
+ }
+ quoteToTweet = encodeURI("\"" + quoteToTweet + "\"");
+ window.open("https://twitter.com/intent/tweet?text=" + quoteToTweet + "&hashtags=GoT,Mustapha");
 }
 
 $(function(){
-    updateQuote();
-    $('#generator').click(updateQuote);
+    updateQuoteSafe();
+    $('#generator').click(updateQuoteSafe);
+    $('#twitter').click(shareTweet);
+    $('#quote').html(displayedQuote.quote);
 })
